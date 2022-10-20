91d88593082edd0caa4c94fdcf3bd58dbd2d1d8f
equipeGrasset <115581337+equipeGrasset@users.noreply.github.com>
2022-10-20 15:54:02 -0400

    Add files via upload

--- utils/db.js
+++ utils/db.js
@@ -6,6 +6,7 @@ async function connect() {
     }
 
     const mariadb = require('mariadb');
+    //const stringConnection = "mariadb://23.235.197.135:daAmbF_TPagrass3t@institutgrassetinfo.com:3306/instit43_portfolio_test";
     const stringConnection = "mariadb://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + '@'
                                           + process.env.DB_HOST + ':' + process.env.DB_PORT + '/'
                                           + process.env.DB_DATA
@@ -15,8 +16,44 @@ async function connect() {
     return connection;
 }
 
+async function findAdmin(username) {
+    const conn = await connect();
+    const sql1 = "SELECT a.administrator_name, a.administrator_surname, u.user_id FROM administrators a INNER JOIN users u ON a.employee_number = u.user_id WHERE user_id = ?"
+    const rows = await conn.query(sql1, [username]);
+    
+    if (rows.length > 0) {
+        return rows[0];
+    } else {
+        return null;
+    }
+}
+
+async function findProfessor(username) {
+    const conn = await connect();
+    const sql1 = "SELECT p.professor_name, p.professor_surname , u.user_id FROM professors p INNER JOIN users u ON p.employee_number = u.user_id WHERE user_id = ?"
+    const rows = await conn.query(sql1, [username]);
+    
+    if (rows.length > 0) {
+        return rows[0];
+    } else {
+        return null;
+    }
+}
+
+async function findStudent(username) {
+    const conn = await connect();
+    const sql1 = "SELECT s.student_name, s.student_surname, u.user_id FROM students s INNER JOIN users u ON s.student_DA = u.user_id WHERE user_id = ?"
+    const rows = await conn.query(sql1, [username]);
+    
+    if (rows.length > 0) {
+        return rows[0];
+    } else {
+        return null;
+    }
+}
+
 async function createUserStudent(username, password, name, surname, qea1, qea2, cb) {
-    const user_password = bcrypt.hashSync(password, 10);
+    const user_password = bcrypt.hashSync(password, 12);
     sql1 = "INSERT INTO users (user_id,user_password,user_role,user_active,user_qea_1,user_qea_2) " + 
 	      "VALUES (?,?,1,0,?,?);"
     global.db.collection("users").insert({username, password: user_password, qea1, qea2}, 
@@ -25,4 +62,4 @@ async function createUserStudent(username, password, name, surname, qea1, qea2, 
         })
 }
 
-module.exports = { connect, createUserStudent }
+module.exports = { connect, createUserStudent, findAdmin, findProfessor, findStudent }
