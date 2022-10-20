7c438c56288fd6f9d299a258633aaa27db4e5889
equipeGrasset <115581337+equipeGrasset@users.noreply.github.com>
2022-10-20 17:35:56 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -16,12 +16,20 @@ function authenticationMiddleware (req, res, next) {
 }
 
 var indexRouter = require('./routes/index');
+
+//var portfolio_vistuer = require('./routes/portfolio-vistuer');
 var usersRouter = require('./routes/users');
+var portfolio_vistuer = require('./routes/portfolio-vistuer');
+ 
+
 
 
 //Etudiant
-var petudinatRouter = require('./routes/petudinat');
-const ModifPetudiantRouter = require('./routes/ModifPetudiant');
+var Etud_portfolio = require('./routes/Etud-portfolio');
+var Etud_Mprofil = require('./routes/Etud-Mprofil');
+var Etud_motPasse = require('./routes/Etud-motPasse');
+var Etud_liste_elem = require('./routes/Etud-liste_elem');
+var Etud_ajout_elem = require('./routes/Etud-ajout_elem');
 
 //Admin
 var admin_Mprofil = require('./routes/admin-Mprofil');
@@ -30,22 +38,16 @@ var admin_lise_Etud = require('./routes/admin-lise_Etud');
 var adminLiseAdmins = require('./routes/admin-lise_admins'); 
 var admin_Lise_Ensg = require('./routes/admin-lise_Ensg');
 var admin_lise_EtudATT = require('./routes/admin-lise_EtudATT'); 
-
+var admin_liste_elem = require('./routes/admin-liste_elem');
 //Enseignant
 var Ensg_lise_Etud = require('./routes/Ensg-lise_Etud');
 var Ensg_Mprofil = require('./routes/Ensg-Mprofil');
+var Ensg_motPasse = require('./routes/Ensg-motPasse');
 
 
 const loginRouter = require('./routes/login');
 const logoutRouter = require('./routes/logout');
 const createAccount = require('./routes/createAccount');
-
-
-//Logins
-const modifierMpOublie = require('./routes/modifierMpOublie');
-const MotDePasseOublieQuestionSecrete = require('./routes/MotDePasseOublieQuestionSecrete');
-const registerEnseignant = require('./routes/registerEnseignant');
-const registerQuestionSecrete = require('./routes/registerQuestionSecrete');
 
 
 
@@ -76,15 +78,6 @@ app.use('/login', loginRouter);
 app.use('/logout', authenticationMiddleware, logoutRouter);
 app.use('/users', authenticationMiddleware, usersRouter);
 
-//logins
-app.use('/modifierMpOublie', modifierMpOublie);
-app.use('/MotDePasseOublieQuestionSecrete', MotDePasseOublieQuestionSecrete);
-app.use('/registerEnseignant', registerEnseignant);
-app.use('/registerQuestionSecrete', registerQuestionSecrete);
-
-
-
-
 //ADmin
 app.use('/admin-Mprofil', authenticationMiddleware, admin_Mprofil);
 app.use('/admin-motPasse', authenticationMiddleware, admin_motPasse); 
@@ -92,17 +85,29 @@ app.use('/admin-lise_EtudATT', authenticationMiddleware, admin_lise_EtudATT);
 app.use('/admin-lise_Etud', authenticationMiddleware, admin_lise_Etud);
 app.use('/admin-lise_Ensg', authenticationMiddleware, admin_Lise_Ensg); 
 app.use('/admin-lise_admins', authenticationMiddleware, adminLiseAdmins);
+app.use('/admin-liste_elem', authenticationMiddleware, admin_liste_elem);
 
 //Enseignant
 app.use('/Ensg-lise_Etud', authenticationMiddleware, Ensg_lise_Etud); 
 app.use('/Ensg-Mprofil', authenticationMiddleware, Ensg_Mprofil); 
+app.use('/Ensg-motPasse', authenticationMiddleware, Ensg_motPasse); 
+
 
 //Etudaint
-app.use('/petudinat', authenticationMiddleware, petudinatRouter);
-app.use('/Modifier-profil-etudiant', authenticationMiddleware, ModifPetudiantRouter);
+app.use('/Etud-portfolio', authenticationMiddleware, Etud_portfolio);
+app.use('/Etud-Mprofil', authenticationMiddleware, Etud_Mprofil);
+app.use('/Etud-motPasse', authenticationMiddleware, Etud_motPasse);
+app.use('/Etud-liste_elem', authenticationMiddleware, Etud_liste_elem);
+app.use('/Etud-ajout_elem', authenticationMiddleware, Etud_ajout_elem);
 
 
-app.use('/', authenticationMiddleware, indexRouter);
+//index portfolio
+app.use('/',  indexRouter);
+app.use('/portfolio-vistuer', portfolio_vistuer);
+
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
