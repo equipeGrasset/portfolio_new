734f5869dec5e347b8d29a06758681a81c72cf46
equipeGrasset <115581337+equipeGrasset@users.noreply.github.com>
2022-10-23 20:13:47 -0400

    Add files via upload

--- routes/admin-lise_admins.js
+++ routes/admin-lise_admins.js
@@ -3,10 +3,50 @@ var router = express.Router();
 
 /* GET portfolio etudinat listing. */
 router.get('/', function(req, res, next) {
-  res.render('admin-lise_admins', { 
-    user: req.user.user_id,
-    title: 'Express'
+  
+  req.getConnection((erreur, connection) => {
+    if (erreur) {
+      console.log(erreur);
+      res.status(500).render("erreur", { erreur });
+    } else {
+      connection.query("select * from administrators", [], (erreur, resultat) => {
+        if (erreur) {
+          console.log(erreur);
+        } else {
+          res.status(200).render("admin-lise_admins", { resultat });
+          console.log(resultat)
+        }
+      });
+    }
+  });
+  
+ 
+});
+
+
+
+router.delete("/admin-lise_admins/:id", (req, res) => {
+  let id = req.params.id;
+  req.getConnection((erreur, connection) => {
+    if (erreur) {
+      console.log(erreur);
+      res.status(500).render("erreur", { erreur });
+    } else {
+      connection.query(
+        "DELETE FROM notes WHERE id = ?",
+        [id],
+        (erreur, resultat) => {
+          if (erreur) {
+            console.log(erreur);
+            res.status(500).render("erreur", { erreur });
+          } else {
+            res.status(200).json({ routeRacine: "/" });
+          }
+        }
+      );
+    }
   });
 });
 
+ 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+ 
+  req.getConnection((erreur, connection) => {
+    if (erreur) {
+      console.log(erreur);
+      res.status(500).render("erreur", { erreur });
+    } else {
+      connection.query("SELECT * FROM students WHERE student_employe_intern_status = 1", [], (erreur, resultat) => {
+        if (erreur) {
+          console.log(erreur);
+        } else {
+          res.status(200).render("index", { resultat });
+          console.log(resultat)
+        }
+      });
+    }
+  });
+
 });
 
 module.exports = router;
